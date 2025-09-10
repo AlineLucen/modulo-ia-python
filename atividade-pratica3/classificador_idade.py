41a67136b080e2ee3bcb9f38288115227995ca12
Aline Silva <alineadslucena@gmail.com>
2025-09-10 11:30:57 -0300

    Classificador de Idade

---
+++ atividade-pratica3/classificador_idade.py
@@ -0,0 +1,11 @@
+# Classicador de Idade
+
+idade = int(input("Digite a sua Idade: "))
+if idade <= 12:
+    print("Você é criança 👶")
+elif idade <= 17:
+      print("Você é adolescemte 👦")
+elif idade <= 59:
+       print("Você é adulto 🧔‍♂️")
+else:
+        print("Você é Idoso 🧓")
